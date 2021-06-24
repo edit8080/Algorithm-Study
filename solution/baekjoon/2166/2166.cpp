92fc5fb6fe22fa61bb822cc9f7011f559cb091ff
Taehee_Lee <leetaehee0205@gmail.com>
2021-06-24 10:57:26 +0900

    2166-다각형의 면적 ★(기하학)

---
+++ solution/baekjoon/2166/2166.cpp
@@ -0,0 +1,35 @@
+﻿/*
+* 1. 사선공식을 통해 입력받은 좌표에 대한 면적을 계산한다.
+*
+* 시간복잡도 = O(n)
+*/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	double area = 0;
+	vector<double> x(n + 1);
+	vector<double> y(n + 1);
+
+	for (int i = 0; i < n; i++)
+		cin >> x[i] >> y[i];
+
+	x[n] = x[0];
+	y[n] = y[0];
+
+	for (int i = 0; i < n; i++)
+		area += x[i] * y[i + 1];
+
+	for (int i = 1; i <= n; i++)
+		area -= x[i] * y[i - 1];
+
+	area = round(abs(area / 2) * 10) / 10.0;
+
+	printf("%.1lf", area);
+}
